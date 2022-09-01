1ac3ca8407df532c00233f322d5ba2ad72d04b91
Skybroad <tianbozhangny@gmail.com>
2022-09-01 09:41:43 -0500

    Cup

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello Wyatt Neal Brumley 2007");
-        System.out.println("Literal Monkey");
-        System.out.println("Bruh");
+        System.out.println(" _________");
+        System.out.println("[         ]");
+        System.out.println("[         ]}}");
+        System.out.println("[~~~~~~~~~]   }");
+        System.out.println("[         ]}}");
+        System.out.println("[ ________]");
     }
 
 }
