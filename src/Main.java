92a3705bacf64f58610c26e854d7629d096876aa
Skybroad <tianbozhangny@gmail.com>
2022-09-01 09:30:42 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Wyatt Neal Brumley 2007");
+        System.out.println("Literal Monkey");
+        System.out.println("Bruh");
+    }
+
+}
